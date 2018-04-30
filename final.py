b9135f9f38c115170bc73dfa7540517edda4e400
fduenez <fduenez@ymail.com>
2018-04-30 15:22:38 -0700

    linking restoreFile to gui

--- final.py
+++ final.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
 from PIL import Image
 from PyQt5.QtGui import QPixmap, QPalette
+import restoreFile as rf
 
 #scroll, zoom
 class ImageExample(QWidget):
@@ -17,30 +18,37 @@ class ImageExample(QWidget):
 		self.buttonFiltering = QPushButton('Filter', self) # Making the filtering button
 		self.buttonFiltering.clicked.connect(self.filteringButtonClicked)
 
-#		self.buttonZooming = QPushButton('Zoom', self) # Making the filtering button
-#		self.buttonZooming.clicked.connect(self.zoomingButtonClicked)
+		self.buttonExit = QPushButton('Exit', self)
+		self.buttonExit.clicked.connect(self.exitButtonClicked)
+
+		self.buttonZooming = QPushButton('Zoom', self) # Making the filtering button
+		self.buttonZooming.clicked.connect(self.zoomingButtonClicked)
 
 		self.picture_label = QLabel(self) # Getting the picture
-		self.my_image = QPixmap('diplo.jpeg')
+		self.my_image = QPixmap('winkled.jpg')
 		self.picture_label.setPixmap(self.my_image)
 
 		hbox = QHBoxLayout() # Making the layout for the buttons
 		hbox.addWidget(self.buttonCropping)
 		hbox.addWidget(self.buttonFiltering)
-
-		vbox = QVBoxLayout() 
+		hbox.addWidget(self.buttonExit)
+		hbox.addWidget(self.buttonZooming)
+		vbox = QVBoxLayout()
 		vbox.addLayout(hbox)
 		vbox.addWidget(self.picture_label)
 
 		self.setLayout(vbox)
-		
-		self.show()
 
+		self.show()
+	def zoomingButtonClicked(self):
+		print("Zoom button clicked")
 	def cropButtonClicked(self): # This function will run when the crop button is click
 		print("Crop Button Clicked")
-
 	def filteringButtonClicked(self): # This function will run when the filter button is click
 		print("Filtering Button Clicked")
+		rf.restore('winkled.jpg')
+	def exitButtonClicked(self): # This function will run when the filter button is click
+		print("Exit Button Clicked")
 
 im = QApplication(sys.argv)
 image_window = ImageExample()
